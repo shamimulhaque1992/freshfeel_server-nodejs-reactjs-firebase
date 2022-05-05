b196fe58e5afaf2fb211f298a3a5fbc6f19a0833
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2022-05-06 04:07:32 +0600

    delete api created and working fine

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ async function run(){
             const result = await productCollection.updateOne(filter,updatedInfo, options);
             res.send(result);
         })
+
+        // deleting a product
+        app.delete('/product/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally{
 
